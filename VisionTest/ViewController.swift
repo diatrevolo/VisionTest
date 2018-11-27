62940c166071c5c2d6e1e6b019205ffe5e61a6d6
Roberto José Osorio-Goenaga <rjo218@gmail.com>
2018-11-26 21:14:47 -0500

    Initial Commit

---
+++ VisionTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VisionTest
+//
+//  Created by Roberto Osorio Goenaga on 11/26/18.
+//  Copyright © 2018 Roberto Osorio Goenaga. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
